8700dbafcd23e3d9742ef1ee9c7ce9b9df638e16
OlegGasiul <oleg.gasiul@informa.com>
2018-03-25 01:37:58 +0100

    Refactoring

---
+++ src/main/java/com/twitter/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.twitter.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.twitter")
+public class AppConfig {
+
+}
